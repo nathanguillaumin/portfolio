052c10df44a38944b29907de1770d556206258e4
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-07-22 18:34:27 +0200

    corrects messages crud

--- views/src/components/Intro.js
+++ views/src/components/Intro.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import API from '../API';
 
 import { withNamespaces } from 'react-i18next';
 
 const Intro = ({t}) => {
+    const [test, setTest] = useState('')
+
+    useEffect(() => {
+        API.get('/projects')
+            .then(res => res.data)
+            .then(data => setTest(data))
+        
+    }, [])
+
+
     return (
         <div>
             <h1>{t('Title Introduction')}</h1>
