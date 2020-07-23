e0d0bcc214c211523add4cf4ec8922c124e30e1b
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-07-23 18:36:25 +0200

    implements message crud in front side

--- views/src/components/AdminHome.js
+++ views/src/components/AdminHome.js
@@ -3,7 +3,10 @@ import API from '../API';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
+import Button from '@material-ui/core/Button';
 import MessageCard from './MessageCard';
+import '../styles/admin-home.css';
+import { NavLink } from 'react-router-dom';
 
 const AdminHome = () => {
   const [messages, setMessages] = useState();
@@ -18,12 +21,22 @@ const AdminHome = () => {
   }, []);
 
   const handlePatch = (id) => {
-    //   API.patch('/messages', { id })
-    //     .then(response => response.data)
-    //   setBookings(messages.filter(b => b.id_booking !== id));
-    //   setValidation(true);
-    //   setIsValidated(true);
-    //   setMsgValidation('La réservation a bien été acceptée et apparaîtra sur votre calendrier de réservation.');
+    API.patch(`/messages/${id}`, { id })
+        .then(response => response.data)
+    setMessages(messages.filter(m => m.id !== id));
+    setValidation(true);
+    setIsValidated(true);
+    setMsgValidation('Le message a bien été marqué comme lu.');
+  };
+
+  const handleDelete = (id) => {
+      console.log(id);
+    API.delete(`/messages/${id}`, { id })
+        .then(response => response.data)
+    setMessages(messages.filter(m => m.id !== id));
+    setValidation(true);
+    setIsValidated(true);
+    setMsgValidation('Le message a bien été supprimé.');
   };
 
   const handleCloseMui = (event, reason) => {
@@ -49,7 +62,8 @@ const AdminHome = () => {
             <MessageCard
               key={m.id}
               m={m}
-              //     handlePatch={handlePatch}
+              handlePatch={handlePatch}
+              handleDelete={handleDelete}
               setMessages={setMessages}
             />
           );
@@ -61,6 +75,12 @@ const AdminHome = () => {
             {msgValidation}
           </Alert>
         </Snackbar>
+
+        <NavLink to='/'>
+            <Button variant="contained" color="primary">
+                Retour au site
+            </Button>
+        </NavLink>
       </div>
     );
   }
